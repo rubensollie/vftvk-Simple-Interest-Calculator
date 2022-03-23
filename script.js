01d29d254b6ef5441823419a11b3841ceced446c
Ruben Sol <r.sol@effecta.nl>
2022-03-23 11:26:02 +0100

    Finish project

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    var inputRate = document.getElementById("rate");
+    var textRate = document.getElementById("rate-text");
+
+    inputRate.addEventListener('input', (ev) => {
+        textRate.innerText = ev.target.value + "%";
+    });
+});
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var inputAmount = parseInt(document.getElementById("principal").value);
+    var inputRate = parseFloat(document.getElementById("rate").value);
+    var inputYears = parseInt(document.getElementById("years").value);
+    document.getElementById("result-amount").innerText = inputAmount.toString();
+    document.getElementById("result-rate").innerText = inputRate.toString() + '%';
+    document.getElementById("result-year").innerText = ((new Date()).getFullYear() + parseInt(inputYears)).toString();
+    document.getElementById("result-value").innerText = ((inputAmount * (inputRate / 100.0)) * inputYears).toString();
+
+    document.getElementById("result").style.display = "block";
 }
         
